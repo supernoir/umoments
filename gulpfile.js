1ed184345b65b08bb832bdff284abc2a8b6a92ea
Conny Kawohl <conny.kawohl@dhcp85.useeds.de>
2016-07-01 12:43:21 +0200

    first commit 2

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+var gulp = require('gulp');
+var webserver = require('gulp-webserver');
+var sass = require('gulp-sass');
+ 
+gulp.task('webserver', function() {
+  gulp.src('./')
+    .pipe(webserver({
+      livereload: true,
+      directoryListing: true,
+      open: true
+    }));
+});
+
+gulp.task('styles', function() {
+    gulp.src('sass/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./css/'));
+});
+
+gulp.task('default',['webserver','styles']);
